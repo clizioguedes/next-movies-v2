899e701414c85f714f22922a778b0ff550e7a787
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-06-22 19:22:03 -0300

    fix: updating unit test

--- src/__tests__/MovieSmCard.spec.tsx
+++ src/__tests__/MovieSmCard.spec.tsx
@@ -2,45 +2,29 @@ import { moviesListMock } from '@/__mocks__/movies';
 import { MovieSmCard } from '@/components/contexts/movies/MovieSmCard';
 import { MoviesContext } from '@/contexts/movies/movies.context';
 import { MovieList } from '@/types/types';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-import { render, screen } from '@testing-library/react';
+const moviesLiked = [] as MovieList[];
 
-describe('MovieSmCard Component', () => {
-  test('render component with favorite movies', () => {
-    const titleMovieMock = moviesListMock[0].title;
+const handleLikeMovies = jest.fn();
 
-    const moviesLikedMock = moviesListMock;
+test('renders movie card correctly', () => {
+  render(
+    <MoviesContext.Provider value={{ moviesLiked, handleLikeMovies }}>
+      <MovieSmCard movie={moviesListMock[0]} />
+    </MoviesContext.Provider>
+  );
 
-    render(
-      <MoviesContext.Provider
-        value={{
-          moviesLiked: moviesLikedMock,
-          handleLikeMovies: () => null,
-        }}
-      >
-        <MovieSmCard movie={moviesListMock[0]} />
-      </MoviesContext.Provider>
-    );
+  const movieTitle = screen.getByText(moviesListMock[0].title);
+  expect(movieTitle).toBeInTheDocument;
 
-    expect(screen.getByText(titleMovieMock)).toBeInTheDocument;
-  });
+  const likeButton = screen.getByText(/Curtir/i);
+  expect(likeButton).toBeInTheDocument;
 
-  test('render component without favorite movies', () => {
-    const titleMovieMock = moviesListMock[0].title;
+  fireEvent.click(likeButton);
 
-    const moviesLikedMock = [] as MovieList[];
-
-    render(
-      <MoviesContext.Provider
-        value={{
-          moviesLiked: moviesLikedMock,
-          handleLikeMovies: () => null,
-        }}
-      >
-        <MovieSmCard movie={moviesListMock[0]} />
-      </MoviesContext.Provider>
-    );
-
-    expect(screen.getByText(titleMovieMock)).toBeInTheDocument;
-  });
+  expect(handleLikeMovies).toHaveBeenCalledWith([
+    ...moviesLiked,
+    { ...moviesListMock[0] },
+  ]);
 });
