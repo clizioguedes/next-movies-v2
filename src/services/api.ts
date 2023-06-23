71eb1bfc048d7c53147dd0ccf29cb1a8f62de5ab
Clizio Guedes <clizioguedes@ioasys.com.br>
2023-06-23 14:21:01 -0300

    refactor: adding .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
     common: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhM2QyZGQxY2Q2MTU5MDdjYWFkNWRjYmRlZWZkM2ZmMyIsInN1YiI6IjY0OTMzNzYxZDIxNDdjMDBhZDUxNTJlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eOpaYEmc6LUJ3LcpRMCKGSXG8G-cozMSmGLhOg_Nkjg',
+      Authorization: `Bearer ${process.env.API_KEY}`,
     },
   },
 });
